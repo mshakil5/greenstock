1de07c6511152fbe8fd3817f1f7f2da87f12acf6
shakil <kazimuhammadullah@gmail.com>
2025-01-08 16:06:15 +0600

    jobs 2

--- resources/views/invoices/print_invoice.blade.php
+++ resources/views/invoices/print_invoice.blade.php
@@ -32,7 +32,7 @@
         }
         .footer {
             position: fixed;
-            bottom: 20px; /* Adjust as needed */
+            bottom: 40px; /* Adjust as needed */
             left: 10px;   /* Adjust as needed */
             font-size: 12px; /* Optional: Adjust for better readability */
             text-align: left; /* Align text to the left */
@@ -41,7 +41,7 @@
         @media print {
             .footer {
                 position: fixed;
-                bottom: 20px;
+                bottom: 40px;
                 left: 10px;
                 text-align: left;
             }
