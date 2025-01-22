504d66773ef9a852069bb1bee513f088500598b9
shakil <kazimuhammadullah@gmail.com>
2025-01-22 12:40:09 +0600

    inv

--- resources/views/invoices/print_invoice.blade.php
+++ resources/views/invoices/print_invoice.blade.php
@@ -146,7 +146,7 @@
 
                 <tr class="total-row">
                     <td colspan="4" style="text-align: right;">Total</td>
-                    <td style="text-align:right">{{ number_format($order->net_total, 2) }}</td>
+                    <td style="text-align:right">{{ number_format($order->grand_total, 2) }}</td>
                 </tr>
                 @if ($order->discount_amount > 0)
                 <tr class="total-row">
